cd9287e816227a427d69f624b5e0be5eef647c51
Fungus-Light <542461670@qq.com>
2022-01-10 18:01:03 +0800

    Rewriter Scanner

--- mustache.ts
+++ mustache.ts
@@ -314,61 +314,65 @@ function nestTokens(tokens) {
  * A simple string scanner that is used by the template parser to find
  * tokens in template strings.
  */
-function Scanner(string) {
-  this.string = string;
-  this.tail = string;
-  this.pos = 0;
-}
-
-/**
+class Scanner {
+  str: string
+  tail: string
+  pos: number
+  constructor(s: string) {
+    this.str = s;
+    this.tail = s;
+    this.pos = 0;
+  }
+  /**
  * Returns `true` if the tail is empty (end of string).
  */
-Scanner.prototype.eos = function eos() {
-  return this.tail === '';
-};
-
-/**
+  eos() {
+    return this.tail === '';
+  };
+  /**
  * Tries to match the given regular expression at the current position.
  * Returns the matched text if it can match, the empty string otherwise.
  */
-Scanner.prototype.scan = function scan(re) {
-  var match = this.tail.match(re);
+  scan(re: RegExp) {
+    let match = this.tail.match(re);
 
-  if (!match || match.index !== 0)
-    return '';
+    if (!match || match.index !== 0)
+      return '';
 
-  var string = match[0];
+    let s = match[0];
 
-  this.tail = this.tail.substring(string.length);
-  this.pos += string.length;
+    this.tail = this.tail.substring(s.length);
+    this.pos += s.length;
 
-  return string;
-};
-
-/**
+    return s;
+  };
+  /**
  * Skips all text until the given regular expression can be matched. Returns
  * the skipped string, which is the entire tail if no match can be made.
  */
-Scanner.prototype.scanUntil = function scanUntil(re) {
-  var index = this.tail.search(re), match;
+  scanUntil(re: RegExp) {
+    let index = this.tail.search(re)
+    let match
 
-  switch (index) {
-    case -1:
-      match = this.tail;
-      this.tail = '';
-      break;
-    case 0:
-      match = '';
-      break;
-    default:
-      match = this.tail.substring(0, index);
-      this.tail = this.tail.substring(index);
-  }
+    switch (index) {
+      case -1:
+        match = this.tail;
+        this.tail = '';
+        break;
+      case 0:
+        match = '';
+        break;
+      default:
+        match = this.tail.substring(0, index);
+        this.tail = this.tail.substring(index);
+    }
 
-  this.pos += match.length;
+    this.pos += match.length;
 
-  return match;
-};
+    return match;
+  };
+}
+
 
 /**
  * Represents a rendering context by wrapping a view object and
